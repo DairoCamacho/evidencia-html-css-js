bb751e8fc138470c6ab9c55151d324a8ae147186
Dairo Camacho <dlcamacho@soy.sena.edu.co>
2024-01-22 12:02:33 -0500

    Refactor del código usando una función separada para reemplazar las clases

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 // FUNCIONALIDAD PARA ACTIVAR EL MODO CLARO y OSCURO
-// Seleccionar el input que está dentro del elemento con la clase switch
+// Seleccionar los dos inputs que están dentro de los elementos con la clase switch
 const inputs = document.querySelectorAll(".switch input[type=checkbox]");
 
 const fondoPrimario = document.querySelectorAll(".fondo-primario");
 const fondoSecundario = document.querySelectorAll(".fondo-secundario");
 const textoPrimario = document.querySelectorAll(".texto-primario");
 const textoSecundario = document.querySelectorAll(".texto-secundario");
-const elementosNavBar = document.querySelectorAll("nav ul a, nav ul li label")
+// const elementosNavBar = document.querySelectorAll("nav ul a, nav ul li label")
+
+// sacamos a parte los foreach en una función independiente
+function reemplazarClase(elementos, clase1, clase2) {
+    elementos.forEach(function (elemento) {
+        elemento.classList.replace(clase1, clase2);
+    });
+}
 
 for (let i = 0; i < inputs.length; i++) {
-    // Añadir un evento de tipo change al input
+    // Capturar el evento de tipo change de los dos inputs
     inputs[i].addEventListener("change", function () {
         // Obtener el valor de la propiedad checked que puede ser true or false
         const estado = inputs[i].checked;
@@ -18,34 +25,17 @@ for (let i = 0; i < inputs.length; i++) {
 
         if (!estado) {
             // Recorrer cada elemento y Reemplazar la clase
-            fondoPrimario.forEach(function (elemento) {
-                elemento.classList.replace("fondo-primario-claro", "fondo-primario");
-            });
-            fondoSecundario.forEach(function (elemento) {
-                elemento.classList.replace("fondo-secundario-claro", "fondo-secundario");
-            });
-            textoPrimario.forEach(function (elemento) {
-                elemento.classList.replace("texto-primario-claro", "texto-primario");
-            });
-            textoSecundario.forEach(function (elemento) {
-                elemento.classList.replace("texto-secundario-claro", "texto-secundario");
-            });
-
+            reemplazarClase(fondoPrimario, "fondo-primario-claro", "fondo-primario");
+            reemplazarClase(fondoSecundario, "fondo-secundario-claro", "fondo-secundario");
+            reemplazarClase(textoPrimario, "texto-primario-claro", "texto-primario");
+            reemplazarClase(textoSecundario, "texto-secundario-claro", "texto-secundario");
         }
         if (estado) {
             // Recorrer cada elemento y Reemplazar la clase
-            fondoPrimario.forEach(function (elemento) {
-                elemento.classList.replace("fondo-primario", "fondo-primario-claro");
-            });
-            fondoSecundario.forEach(function (elemento) {
-                elemento.classList.replace("fondo-secundario", "fondo-secundario-claro");
-            });
-            textoPrimario.forEach(function (elemento) {
-                elemento.classList.replace("texto-primario", "texto-primario-claro");
-            });
-            textoSecundario.forEach(function (elemento) {
-                elemento.classList.replace("texto-secundario", "texto-secundario-claro");
-            });
+            reemplazarClase(fondoPrimario, "fondo-primario", "fondo-primario-claro");
+            reemplazarClase(fondoSecundario, "fondo-secundario", "fondo-secundario-claro");
+            reemplazarClase(textoPrimario, "texto-primario", "texto-primario-claro");
+            reemplazarClase(textoSecundario, "texto-secundario", "texto-secundario-claro");
         }
     });
 }
